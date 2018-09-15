98a3ef2909064461ef3843ca521479707a4312a7
alborrajo <alvarolopezborr@gmail.com>
2018-09-16 00:58:17 +0200

    Restored file upload and directory making functionality (hotfix 8)

--- panel/objects/Dir.php
+++ panel/objects/Dir.php
@@ -122,6 +122,8 @@ class Dir{
 										data.append('action',"upload");
 										data.append('dir',"<?php echo $this->relPath;?>");
 
+										console.log(data);
+
 										var request = new XMLHttpRequest();
 										request.onreadystatechange = function(){
 											if(request.readyState == 4) {
