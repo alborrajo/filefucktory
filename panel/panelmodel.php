1d4a70092e59ec3419e3e09b1f4232538a29d768
alborrajo <alvarolopezborr@gmail.com>
2018-09-16 00:32:36 +0200

    Restored file upload and directory making functionality (hotfix 4)

--- panel/panelmodel.php
+++ panel/panelmodel.php
@@ -12,7 +12,7 @@ class PanelModel {
 		//Check entries for a match
 		foreach($db->users as $user) {
 			if($user->email == $email) {
-				$usedmb = GetDirectorySize("files/".$_SESSION["userFolder"])/1048576; //Bytes to MB
+				$usedmb = $this->GetDirectorySize("files/".$_SESSION["userFolder"])/1048576; //Bytes to MB
 				$space = array("usedmb"=>$usedmb, "spacemb"=>$user->spacemb);
 			}
 		}
